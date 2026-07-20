fae3981aaaa43b62c9e188ad800b3d7754e65d7b
Vaish"nav <153205209+vaish-nav-sahil@users.noreply.github.com>
2026-07-20 22:28:03 +0530

    Update 15thJuly3658.cpp
    added one more solution with logic

--- July_Leetcode_2026/15thJuly3658.cpp
+++ July_Leetcode_2026/15thJuly3658.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     // Brute Force Approach -> 
@@ -22,7 +25,32 @@ public:
         }
 
         return gcd;
+    }
 
 
+// Approach 2-> 
+// t.c -> O(1)
+// s.c -> O(1)
+// This also works can you guess why ?
+// Sum of first n odd natural numbers -> n^2
+// Sum of first n even natural numbers -> n(n+1)
+
+// now we have to find gcd of (n^2 , n(n+1))
+//                    gcd of n(n , n+1)
+//                    gcd of n(gcd of n & n+1)
+//                    gcd of n(1)
+//                    gcd is n
+
+    int gcdOfOddEvenSums(int n) {
+        return n ;
     }
 };
+
+
+
+int main(){
+    int n;
+    cin>>n;
+    Solution obj;
+    cout<<obj.gcdOfOddEvenSums(n);
+}
